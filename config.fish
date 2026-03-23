8c13330743734d4d05e33865c3f88d4711d204d8
Drix <itsdrixhere@gmail.com>
2026-03-23 22:37:28 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,14 +2,27 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 git -C ~/.config/fish pull 2>/dev/null > /dev/null &
 
-"color": {
-    "1": "38;2;255;0;0",
-    "2": "38;2;0;255;0",
-    "3": "38;2;0;0;255"
-}
+set aquaSqueeze = "38;2;255;0;255"    # white       1
+set mirage = "38;2;255;0;255"         # base        2
+set violetBlue = "38;2;255;0;255"     # purple      3
+set paleSkyBlue = "38;2;255;0;255"    # light blue  4
+set pictonBlue = "38;2;255;0;255"     # blue        5
+set periwinkleBlue = "38;2;255;0;255" # dark blue   6
+set bittersweet = "38;2;255;0;255"    # light red   7
+set lava = "38;2;255;0;255"           # red         8
+set darkRed = "38;2;255;0;255"        # dark red    9
 
 function fish_greeting
-   fastfetch --file  $(random ~/.config/fish/logos/Drix*.txt)
+   fastfetch --file  $(random ~/.config/fish/logos/Drix*.txt) \
+        --logo-color-1 "aquaSqueeze" \
+        --logo-color-2 "mirage" \
+        --logo-color-3 "violetBlue" \
+        --logo-color-4 "paleSkyBlue" \
+        --logo-color-5 "pictonBlue" \
+        --logo-color-6 "periwinkleBlue" \
+        --logo-color-7 "bittersweet" \
+        --logo-color-8 "lava" \
+        --logo-color-9 "darkRed"
    # hyfetch --distro $(random choice Furreto YiffOS)
 end
 
