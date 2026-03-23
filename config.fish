dd7e01964b2619c8188d8338b99b64bdd154d2c4
Drix <itsdrixhere@gmail.com>
2026-03-23 22:42:28 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,15 +2,15 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 git -C ~/.config/fish pull 2>/dev/null > /dev/null &
 
-set aquaSqueeze = "38;2;255;0;255"    # white       1
-set mirage = "38;2;255;0;255"         # base        2
-set violetBlue = "38;2;255;0;255"     # purple      3
-set paleSkyBlue = "38;2;255;0;255"    # light blue  4
-set pictonBlue = "38;2;255;0;255"     # blue        5
-set periwinkleBlue = "38;2;255;0;255" # dark blue   6
-set bittersweet = "38;2;255;0;255"    # light red   7
-set lava = "38;2;255;0;255"           # red         8
-set darkRed = "38;2;255;0;255"        # dark red    9
+set aquaSqueeze = "38;2;233;243;247"     # white       1
+set mirage = "38;2;26;14;36"             # base        2
+set violetBlue = "38;2;81;10;201"        # purple      3
+set paleSkyBlue = "38;2;189;246;254"     # light blue  4
+set pictonBlue = "38;2;69;177;232"       # blue        5
+set periwinkleBlue = "38;2;143;153;251"  # dark blue   6
+set bittersweet = "38;2;254;111;94"      # light red   7
+set lava = "38;2;207;16;32"              # red         8
+set darkRed = "38;2;132;0;0"             # dark red    9
 
 function fish_greeting
    fastfetch --file  $(random ~/.config/fish/logos/Drix*.txt) \
